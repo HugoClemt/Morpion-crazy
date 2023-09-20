7b03e55d96995326473c6e3bed704261ad867154
Valentin <valentin.renaudin@my-digital-school.org>
2023-09-20 16:53:31 +0200

    Ajout Valentin vers main

--- tic.js
+++ tic.js
@@ -6,97 +6,57 @@
 //On intialise l'état à 0, il changera à chaque clic sur une case
 var state = 0;
 
-//On déclare un tableau de var qui vont récupérer les id de chaque case
-let d1, d2, d3, d4, d5, d6, d7, d8, d9;
-let cases = [d1, d2, d3, d4, d5, d6, d7, d8, d9];
-
-//On boucle sur le tableau pour associer chaque var à son id
-for (let o = 0; o < cases.length; o++) {
-    cases[o] = document.getElementById(o);
-}
-
-// let modal = document.getElementById(exampleModal);
-// let modall = document.querySelector("#exampleModal");
-//console.log(cases[0]);
-/** 
-var c1, c2, c3, c4, c5, c6, c7, c8, c9;
-c1 = document.getElementById("case-1");
-c2 = document.getElementById("case-2");
-c3 = document.getElementById("case-3");
-c4 = document.getElementById("case-4");
-c5 = document.getElementById("case-5");
-c6 = document.getElementById("case-6");
-c7 = document.getElementById("case-7");
-c8 = document.getElementById("case-8");
-c9 = document.getElementById("case-9");
-**/
+let cases = document.querySelectorAll(".case");
 
 //Méthode qui effectue un coup X ou Y en fonction de l'état
 //On vérifie que la case soit bien vide
 function exec(casee) {
+  if (state == 0 && casee.value != "X" && casee.value != "O") {
+    casee.value = "X";
+    state = 1;
+  } else if (state == 1 && casee.value != "X" && casee.value != "O") {
+    casee.value = "O";
+    state = 0;
+  }
 
-    if (state == 0 && casee.value != 'X' && casee.value != 'O') {
-        casee.value = "X";
-        state = 1;
-    }
-
-    else if (state == 1 && casee.value != 'X' && casee.value != 'O') {
-        casee.value = "O";
-        state = 0;
-    }
-
-    //On vient tester toutes les possibilités de victoire 
-    //On teste si X a gagné
-    if (cases[0].value == 'X' && cases[1].value == 'X' && cases[2].value == 'X'
-        || cases[3].value == 'X' && cases[4].value == 'X' && cases[5].value == 'X'
-        || cases[6].value == 'X' && cases[7].value == 'X' && cases[8].value == 'X'
-        || cases[0].value == 'X' && cases[3].value == 'X' && cases[6].value == 'X'
-        || cases[1].value == 'X' && cases[4].value == 'X' && cases[7].value == 'X'
-        || cases[2].value == 'X' && cases[5].value == 'X' && cases[8].value == 'X'
-        || cases[0].value == 'X' && cases[4].value == 'X' && cases[8].value == 'X'
-        || cases[2].value == 'X' && cases[4].value == 'X' && cases[6].value == 'X') {
-        console.log("Le joueur X a gagné");
-        showModal();
-
-
-    }
-
-    //On teste si O a gagné
-    else if (cases[0].value == 'O' && cases[1].value == 'O' && cases[2].value == 'O'
-    || cases[3].value == 'O' && cases[4].value == 'O' && cases[5].value == 'O'
-    || cases[6].value == 'O' && cases[7].value == 'O' && cases[8].value == 'O'
-    || cases[0].value == 'O' && cases[3].value == 'O' && cases[6].value == 'O'
-    || cases[1].value == 'O' && cases[4].value == 'O' && cases[7].value == 'O'
-    || cases[2].value == 'O' && cases[5].value == 'O' && cases[8].value == 'O'
-    || cases[0].value == 'O' && cases[4].value == 'O' && cases[8].value == 'O'
-    || cases[2].value == 'O' && cases[4].value == 'O' && cases[6].value == 'O') {
-        console.log("Le joueur O a gagné");
-    }
-
-    else {
-        // console.log("Partie terminée");
-        // showModal();
-    }
-    //isDone();
-
+  //On vient tester toutes les possibilités de victoire
+  //On teste si X a gagné
+  if (checkWin('X')) {
+    console.log("Le joueur X a gagné!")
+  }
+  else if (checkWin('O')) {
+    console.log("Le joueur O a gagné !")
+  }
 }
 
-
-function isDone() {
-    for (let i = 0; i < cases.length; i++) {
-        console.log(cases[i].value)
-        if (cases[i].value == "") {
-            return false;
-        }
-        else {
-            console.log("FIN DE LA PARTIE");
-            return true;
-        }
+function checkWin(player) {
+  for (let i = 0; i < 3; i++) {
+    if (
+      cases[i * 3].value === player &&
+      cases[i * 3 + 1].value === player &&
+      cases[i * 3 + 2].value === player
+    ) {
+      return true;
     }
+    if (
+      cases[i].value === player &&
+      cases[i + 3] === player &&
+      cases[i + 6] === player
+    ) {
+      return true;
+    }
+
+    if (
+      (cases[0].value === player &&
+        cases[4].value === player &&
+        cases[8].value === player) ||
+      (cases[2].value === player &&
+        cases[4].value === player &&
+        cases[6].value === player)
+    ) {
+      return true;
+    }
+
+    return false;
+  }
 }
-
-
-/* function showModal() {
- modall.style.visibility = "visible";   
-//  modall.style.z-index =1;
-}   */
