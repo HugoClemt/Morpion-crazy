ea09fc5888cfec4b83e173306488b0a2ea457ba5
HugoClemt <hugoclemt@gmail.com>
2023-09-21 09:29:26 +0200

    modif classement avec la nouvelle connexion bdd

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Morpion Crazy - DEV</title>
   <link rel="stylesheet" href="./css/main.css">
-  <link href="./config/cnx.php" />
+  <link href="./" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
