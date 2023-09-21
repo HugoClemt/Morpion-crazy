4ae405b0be444fde308d7e6390dfdf5961c9800d
Elberby <jjulien.jjouanne@gmail.com>
2023-09-21 15:48:15 +0200

    scss

--- view/components/demo-grille.php
+++ view/components/demo-grille.php
@@ -1,22 +1,24 @@
-<div class="row quadrillage">
-    <div class="col">
-        <input class="case" type="text" id="case-0" onclick="exec(cases[0]);" readonly value="">
-        <input class="case" type="text" id="case-1" onclick="exec(cases[1]);" readonly value="">
-        <input class="case" type="text" id="case-2" onclick="exec(cases[2])" readonly>
+<div class="grille">
+    <div class="row quadrillage">
+        <div class="col">
+            <input class="case" type="text" id="case-0" onclick="exec(cases[0]);" readonly value="">
+            <input class="case" type="text" id="case-1" onclick="exec(cases[1]);" readonly value="">
+            <input class="case" type="text" id="case-2" onclick="exec(cases[2])" readonly>
+        </div>
     </div>
-</div>
-<div class="row quadrillage">
-    <div class="col">
-        <input class="case" type="text" id="case-3" onclick="exec(cases[3]);" readonly>
-        <input class="case" type="text" id="case-4" onclick="exec(cases[4]);" readonly>
-        <input class="case" type="text" id="case-5" onclick="exec(cases[5]);" readonly>
+    <div class="row quadrillage">
+        <div class="col">
+            <input class="case" type="text" id="case-3" onclick="exec(cases[3]);" readonly>
+            <input class="case" type="text" id="case-4" onclick="exec(cases[4]);" readonly>
+            <input class="case" type="text" id="case-5" onclick="exec(cases[5]);" readonly>
+        </div>
     </div>
-</div>
 
-<div class="row quadrillage">
-    <div class="col last-col">
-        <input class="case" type="text" id="case-6" onclick="exec(cases[6]);" readonly>
-        <input class="case" type="text" id="case-7" onclick="exec(cases[7]);" readonly>
-        <input class="case" type="text" id="case-8" onclick="exec(cases[8]);" readonly>
+    <div class="row quadrillage">
+        <div class="col last-col">
+            <input class="case" type="text" id="case-6" onclick="exec(cases[6]);" readonly>
+            <input class="case" type="text" id="case-7" onclick="exec(cases[7]);" readonly>
+            <input class="case" type="text" id="case-8" onclick="exec(cases[8]);" readonly>
+        </div>
     </div>
 </div>
--- view/template/template-page.php
+++ view/template/template-page.php
@@ -1,26 +1,28 @@
 <div class="row container-principale">
-    <div class="col-6">
-        <div class="row" id="block-left-top">
-            <div class="col" id="modeJeuxContainer">
-                <?php
-                $page = isset($_GET['page']) ? $_GET['page'] : 'jouer';
+    <div class="col-6 d-flex align-items-center">
+        <div class="contain">
+            <div class="row" id="block-left-top">
+                <div class="col" id="modeJeuxContainer">
+                    <?php
+                    $page = isset($_GET['page']) ? $_GET['page'] : 'jouer';
 
-                switch ($page) {
-                    case 'jouer':
-                        include('./view/components/jouer.php');
-                        break;
-                    case 'mode-jeux':
-                        include('./view/components/mode-jeux.php');
-                        break;
-                }
-                ?>
+                    switch ($page) {
+                        case 'jouer':
+                            include('./view/components/jouer.php');
+                            break;
+                        case 'mode-jeux':
+                            include('./view/components/mode-jeux.php');
+                            break;
+                    }
+                    ?>
+                </div>
             </div>
-        </div>
-        <div class="row" id="block-left-bottom">
-            <div class="col">
-                <?php
-                include('./view/components/classement.php');
-                ?>
+            <div class="row" id="block-left-bottom">
+                <div class="col">
+                    <?php
+                    include('./view/components/classement.php');
+                    ?>
+                </div>
             </div>
         </div>
     </div>
