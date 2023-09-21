f1e85fc78c8d87fa9d04332c01326338e34a4827
Elberby <jjulien.jjouanne@gmail.com>
2023-09-21 09:11:44 +0200

    bdd

--- view/components/mode-jeux.php
+++ view/components/mode-jeux.php
@@ -18,8 +18,8 @@
         <form label="infoMulti" method="post" action="multi.php">
             <div class="collapse" id="modalMulti">
                 <div class="card card-body">
-                    <input type="text" name="pseudoMulti1" id="pseudoMulti1" placeholder="Entrez le premier pseudo">
-                    <input type="text" name="pseudoMulti2" id="pseudoMulti2" placeholder="Entrez le second pseudo">
+                    <input type="text" name="pseudoMulti1" id="pseudoMulti1" placeholder="Entrez le premier pseudo" required="required">
+                    <input type="text" name="pseudoMulti2" id="pseudoMulti2" placeholder="Entrez le second pseudo" required="required">
 
                 </div>
             <button class="btn btn-primary" type="submit" >Jouer</button>
