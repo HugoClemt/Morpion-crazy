87a8a0b1255820196753c978f6163c2360be7ba8
Elberby <jjulien.jjouanne@gmail.com>
2023-09-21 16:41:59 +0200

    traitement name solo

--- controller/add_pseudo.php
+++ controller/add_pseudo.php
@@ -1,9 +1,5 @@
 <?php
 include_once './model/hight_score.php';
 
-// function add_pseudo($pseudo, $point)  {
-
-// }
-
 
 
--- model/hight_score.php
+++ model/hight_score.php
@@ -1,6 +1,8 @@
 <?php
 include_once __DIR__ . '/database.php';
 
+//Cette requête SQL extrait les noms et les points associés des trois meilleurs scores
+// Utilise la fonction "databaseRead" pour exécuter une requête SQL de lecture
 $hight_score = databaseRead(
     "SELECT name, point FROM score ORDER BY point DESC
 LIMIT 3;"
